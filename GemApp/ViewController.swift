856c3a64a3c06d153cdbbba63d884e22f4f456eb
MacBook Pro <MacBook@sandips-mbp.lan>
2020-09-15 21:16:21 -0400

    Initial Commit

---
+++ GemApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GemApp
+//
+//  Created by MacBook Pro on 9/15/20.
+//  Copyright © 2020 Shannon Firooz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
